63629471d76ff72304d094040d38e4a3ef9e4612
Sourabh Panda <Sourabh.Panda@Worley.com>
2022-08-02 09:54:45 +0000

    Updated main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import pyodbc
+print("Hello from Sourabh")
 server = 'sgsin01sql001v.database.windows.net'
 database = 'control-tower-test-db'
 username = 'sourabh'
 password = '{admin@123}'   
 driver= '{FreeTDS}'
 
-with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+conn = pyodbc.connect('DRIVER={FreeTDS};SERVER=sgsin01sql001v.database.windows.net;DATABASE=control-tower-test-db;UID=sourabh;PWD=admin@123', autocommit=True)
+
+#with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
         cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
         row = cursor.fetchone()
