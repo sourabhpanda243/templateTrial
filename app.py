a6564091866b11f60378895ed91205b5f00f73d1
Sourabh Panda <Sourabh.Panda@Worley.com>
2022-08-03 06:19:14 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ driver= '{FreeTDS}'
 conn = pyodbc.connect('DRIVER={FreeTDS};SERVER=sgsin01sql001v.database.windows.net;PORT=1433;DATABASE=control-tower-test-db;UID=sourabh;PWD=admin@123', autocommit=True)
 
 #with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-    with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+with conn.cursor() as cursor:
+    cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+    row = cursor.fetchone()
+    while row:
+        print (str(row[0]) + " " + str(row[1]))
         row = cursor.fetchone()
-        while row:
-            print (str(row[0]) + " " + str(row[1]))
-            row = cursor.fetchone()
