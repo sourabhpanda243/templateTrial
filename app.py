88ae3ab985ce0a426590a24a275fcb5ebd10d8a0
Sourabh Panda <Sourabh.Panda@Worley.com>
2022-08-08 07:15:26 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ username = 'sourabh'
 password = '{admin@123}'   
 driver= '{FreeTDS}'
 
-conn = pyodbc.connect('DRIVER={FreeTDS};SERVER=sgsin01sql001v.database.windows.net;PORT=1433;DATABASE=control-tower-test-db;UID=sourabh;PWD=admin@123', autocommit=True)
+#conn = pyodbc.connect('DRIVER={FreeTDS};SERVER=sgsin01sql001v.database.windows.net;PORT=1433;DATABASE=control-tower-test-db;UID=sourabh;PWD=admin@123', autocommit=True)
 
-#with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
-with conn.cursor() as cursor:
-    cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
-    row = cursor.fetchone()
-    while row:
-        print (str(row[0]) + " " + str(row[1]))
+with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
+    with conn.cursor() as cursor:
+        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
         row = cursor.fetchone()
+        while row:
+            print (str(row[0]) + " " + str(row[1]))
+            row = cursor.fetchone()
