e6d883b010a8ad65ba028b69240ab571d383f102
Sourabh Panda <Sourabh.Panda@Worley.com>
2022-08-16 11:50:56 +0000

    Updated app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ driver= '{ODBC Driver 18 for SQL Server}'
 
 with pyodbc.connect('DRIVER='+driver+';SERVER=tcp:'+server+';PORT=1433;DATABASE='+database+';UID='+username+';PWD='+ password) as conn:
     with conn.cursor() as cursor:
-        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+#        cursor.execute("SELECT TOP 3 name, collation_name FROM sys.databases")
+        cursor.execute("select * from dbo.Persons")
         row = cursor.fetchone()
         while row:
             print (str(row[0]) + " " + str(row[1]))
